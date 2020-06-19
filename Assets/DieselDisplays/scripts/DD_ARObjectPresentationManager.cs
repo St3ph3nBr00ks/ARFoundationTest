0d43195a3024a9e08c5d56b5489e757325aa7431
St3ph3nBr00ks <stephenbrooks187@att.net>
2020-06-19 00:44:16 -0500

    Accidently uploaded arcore version of project before, changing to ARFoundation

---
+++ Assets/DieselDisplays/scripts/DD_ARObjectPresentationManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DD_ARObjectPresentationManager : MonoBehaviour
+{
+    public Transform presentationObj;
+    
+    int i = 0;
+    void Update()
+    {
+        if (FindObjectOfType<DD_Debug>().debugText)
+        {
+            //debugText.text = "transform.eulerAngles.x : " + transform.eulerAngles.x + " presentationObj.localEulerAngles : " + presentationObj.localEulerAngles;
+        }
+
+        if (Mathf.Abs(transform.eulerAngles.x) < 45 || 100 < Mathf.Abs(transform.eulerAngles.x))
+        {
+
+            if (FindObjectOfType<DD_Debug>().debugText)
+            {
+                FindObjectOfType<DD_Debug>().debugText.text = 
+                    "transform.eulerAngles.x : " + transform.eulerAngles.x + "\n" + " " +
+                    "presentationObj.localEulerAngles : " + presentationObj.localEulerAngles + "\n" + " " +
+                    "rotate presentation obj " + i++;
+            }
+            presentationObj.localEulerAngles = new Vector3(-45,0,0); 
+        }
+        else
+        {
+            if (FindObjectOfType<DD_Debug>().debugText)
+            {
+                FindObjectOfType<DD_Debug>().debugText.text = " flatten presentation obj ";
+            }
+           // presentationObj.eulerAngles = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/DieselDisplays/scripts/DD_NullParent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DD_NullParent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.SetParent(null);
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        transform.SetParent(null);
+    }
+}
